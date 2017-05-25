fdd8f81054b285246765611750a724ec6159b4a9
DESKTOP-JQ61JFE\Park Jaemin <a85828187@gmail.com>
2017-05-25 18:29:35 +0900

    예외상황 발생 시 가장 먼저 interrupt 신호를 보내도록 변경 아무 일 없을 때 interrupt를 0으로 만듬

--- script.py
+++ script.py
@@ -13,8 +13,13 @@ while 1:
     time.sleep(1)
     i2b.update()
     if PathMap.is_blocked(i2b):
-        print "path update"
-        PathMap.update(i2b)
-        f_interrupt = open("interrupt.txt", 'w')
+        f_interrupt = open("interrupt.txt",'w')
         f_interrupt.write("1")
         f_interrupt.close()
+        print "path update"
+        PathMap.update(i2b)
+    else:
+        f_interrupt = open("interrupt.txt",'w')
+        f_interrupt.write("0")
+        f_interrupt.close()
+
