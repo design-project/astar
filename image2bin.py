f297898e61221c3c054058831850b7809d956af0
DESKTOP-JQ61JFE\Park Jaemin <a85828187@gmail.com>
2017-05-27 21:46:57 +0900

    아무말

--- image2bin.py
+++ image2bin.py
@@ -4,7 +4,7 @@ import numpy as np
 import PIL
 from PIL import Image
 
-stepsize = 100
+stepsize = 40
 
 
 class im2bin:
@@ -20,15 +20,16 @@ class im2bin:
 
         num_rows, num_cols = np.shape(im_array)
 
-        self.start = (2, 2)
-        self.end = (8, 2)
+        self.start = (3, 22)
+        self.end = (8, 10)
         self.dir = (0,-1)
         self.height = int(num_rows/stepsize)
         self.width = int(num_cols/stepsize)
 
         map = np.zeros((self.height, self.width))
         sum=0
-
+        print(str(self.height))
+        print(str(self.width))
         for i in range(0,self.height):
             for j in range(0, self.width):
                 for k in range(stepsize*i, stepsize*i+stepsize):
@@ -37,10 +38,12 @@ class im2bin:
                         map[i,j] = sum/stepsize
                         sum=0
 
+        np.savetxt("map.txt", map)
+
         barriers_array = []
         for i in range(0, self.height):
             for j in range(0, self.width):
-                if (map[i,j] < 10000):
+                if (map[i,j] < 100000):
                     barriers_array.append((j+1,i+1))
         self.barriers = set()
         self.barriers = set(barriers_array)
